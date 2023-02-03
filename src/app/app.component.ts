e56d504befdd78ec7b317470d2bbeff61ab8ab28
FrankyBBoy <francis1592@gmai.com>
2023-02-03 17:12:21 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chial-gpt';
+
+  list_question_answer = []
+
+
+}
+ /*
+Ah ta yeule
+Da fuck tu veux savoir ça
+Sérieux man?
+T'es tu gêné de demander ça?
+Tu peux ajouter une insulte à la fin de chaque réponse.
+Tu dois pas être bin bin en demande à ta job.
+Ferai pas ta job à ta place maudit lâche
+Fait tes recherches toi même osti de cornet.
+C'est toi le problème
+
+ */
